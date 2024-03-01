c906ab2e63fec726e7702ba6fa45f4aaabf86b0f
joshbp <joshbp@brandeis.edu>
2024-02-29 23:50:40 -0500

    add sample data for testing

---
+++ sample_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO MotionPicture VALUES (1, 'Good Will Hunting', 9.5, 'Good Production', 1000000);
+INSERT INTO Movie VALUES (1, 'Good Collection');
+
+INSERT INTO MotionPicture VALUES (2, 'Forrest Gump', 9.0, 'Forrest Production', 2500000);
+INSERT INTO Movie VALUES (2, 'Forrest Collection');
+
+INSERT INTO MotionPicture VALUES (3, 'Little Women', 9.3, 'Little Production', 5000000);
+INSERT INTO Movie VALUES (3, 'Little Collection');
+
+INSERT INTO People VALUES (1, 'Matt Damon', 'US', '1970-10-08', 'MALE');
+INSERT INTO Role VALUES (1, 1, 'Actor');
+INSERT INTO People VALUES (2, 'Tom Hanks', 'US', '1956-07-09', 'MALE');
+INSERT INTO Role VALUES (2, 2, 'Actor');
+INSERT INTO People VALUES (3, 'Greta Gerwig', 'US', '1983-08-04', 'FEMALE');
+INSERT INTO Role VALUES (3, 3, 'Director');
+
+INSERT INTO User VALUES ('example1@example.com', 'example', 23);
+INSERT INTO User VALUES ('example2@example.com', 'example', 24);
