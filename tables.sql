0da13ade07d942122354c52ea5f292b7a3da177b
joshbp <joshbp@brandeis.edu>
2024-03-20 15:58:13 -0400

    add data and fix table data column types to match

--- tables.sql
+++ tables.sql
@@ -1,72 +1,72 @@
 CREATE TABLE MotionPicture (
-        id INTEGER,
-        name CHAR(20),
+        id CHAR(3),
+        name CHAR(50),
         rating REAL,
-        production CHAR(20),
-        budget INTEGER,
-        PRIMARY KEY (id))
+        production CHAR(50),
+        budget BIGINT,
+        PRIMARY KEY (id));
 
 CREATE TABLE User (
         email CHAR(100),
         name CHAR(20),
         age INTEGER,
-        PRIMARY KEY (email))
+        PRIMARY KEY (email));
 
 CREATE TABLE Likes (
+        mpid CHAR(3),
         uemail CHAR(100),
-        mpid INTEGER,
         PRIMARY KEY (uemail, mpid),
         FOREIGN KEY (uemail) REFERENCES User(email),
-        FOREIGN KEY (mpid) REFERENCES MotionPicture(id))
+        FOREIGN KEY (mpid) REFERENCES MotionPicture(id));
 
 CREATE TABLE Movie (
-        mpid INTEGER NOT NULL,
+        mpid CHAR(3) NOT NULL,
         boxoffice_collection CHAR(20),
         PRIMARY KEY (mpid),
-        FOREIGN KEY (mpid) REFERENCES MotionPicture(id))
+        FOREIGN KEY (mpid) REFERENCES MotionPicture(id));
 
 CREATE TABLE Series (
-        mpid INTEGER NOT NULL,
+        mpid CHAR(3) NOT NULL,
         season_count INTEGER,
         PRIMARY KEY (mpid),
-        FOREIGN KEY (mpid) REFERENCES MotionPicture(id))
+        FOREIGN KEY (mpid) REFERENCES MotionPicture(id));
 
 CREATE TABLE People (
-        id INTEGER,
-        name CHAR(20),
-        nationality CHAR(20),
+        id CHAR(2),
+        name CHAR(50),
+        nationality CHAR(50),
         dob DATE,
-        gender CHAR(20),
-        PRIMARY KEY (id))
+        gender CHAR(1),
+        PRIMARY KEY (id));
 
 CREATE TABLE Role (
-        mpid INTEGER,
-        pid INTEGER,
+        mpid CHAR(3),
+        pid CHAR(2),
         role_name CHAR(20),
         PRIMARY KEY (mpid, pid, role_name),
         FOREIGN KEY (mpid) REFERENCES MotionPicture(id),
-        FOREIGN KEY (pid) REFERENCES People(id))
+        FOREIGN KEY (pid) REFERENCES People(id));
 
 CREATE TABLE Award (
-        mpid INTEGER,
-        pid INTEGER,
-        award_name CHAR(20),
+        mpid CHAR(3),
+        pid CHAR(2),
+        award_name CHAR(50),
         award_year INTEGER,
         PRIMARY KEY (mpid, pid, award_name, award_year),
         FOREIGN KEY (mpid) REFERENCES MotionPicture(id),
-        FOREIGN KEY (pid) REFERENCES People(id))
+        FOREIGN KEY (pid) REFERENCES People(id));
 
 CREATE TABLE Genre (
-        mpid INTEGER,
+        mpid CHAR(3),
         genre_name CHAR(20),
         PRIMARY KEY (mpid, genre_name),
-        FOREIGN KEY (mpid) REFERENCES MotionPicture(id))
+        FOREIGN KEY (mpid) REFERENCES MotionPicture(id));
 
 CREATE TABLE Location (
-        mpid INTEGER NOT NULL,
-        zip CHAR(5),
-        city CHAR(20),
-        country CHAR(20),
+        mpid CHAR(3) NOT NULL,
+        zip CHAR(10),
+        city CHAR(50),
+        country CHAR(50),
         PRIMARY KEY (mpid, zip),
         FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
-        ON DELETE CASCADE)
+        ON DELETE CASCADE);
